199cf512a808a85978ab417077f8368df77ef660
BrowningL <71730323+BrowningL@users.noreply.github.com>
2020-11-24 21:42:54 +0000

    Update tweetScraper.py
    V.1.2
    -Fixed count error

--- tweetScraper.py
+++ tweetScraper.py
@@ -2,13 +2,12 @@ import tweepy
 import time
 from tweetPoster import *
 
-
 # Authenticate account @SentryFeed to Twitter // Set variables
 auth = tweepy.OAuthHandler("TaKRCVSWQwbuoro6urJ5yPszb", "EPneGfRaOLiKdRzhlNJ6t6NJ311q2DrINonkZ6HnTrm1e5fnxq")
 auth.set_access_token("1329106117356609537-FoI7VGBMW4biy4pPGGyTf5y3LtDsdM", "D3V9tbPZSUuPb1p2QZvYcMmWdJmtCOp5lruyJA21BXLhY")
 # wait_on_rate_limit_notify=True, This will print a notification when download limit is reached
 api = tweepy.API(auth, wait_on_rate_limit_notify=True)
-userID = 'SentryFeed'
+userID = 'crypDat_io'
 update = True
 empty = False
 APIcount = 0
@@ -17,17 +16,9 @@ tweetCount = 0
 logFrequency = 20
 start_time = time.time()
 
-def oldTweets():
-    for tweet in tweets:
-        oldTweet1 = tweet
-        if tweet = oldTweet1:
-
-
-
 while empty == False:
-    #Count will determine how many tweets are returned
     tweets = api.user_timeline(screen_name = userID,
-        count = 2,
+        count = 1,
         tweet_mode = "extended"
         )
     APIcount += 1
@@ -60,34 +51,3 @@ while empty == False:
         print("Bot has made:",APIcount,"API calls and made",tweetCount,"tweets in the last",logFrequency,"minutes")
         logFrequency += logFrequency
 
-"""
-
-#Class to set up the streaming object, plus error handling
-class Listener(StreamListener):
-
-    def on_status(self, status):
-        print("Tweeting: ", status.text)
-        return(True)
-
-    def on_error(self, status_code):
-        print("Encountered streaming error: (", status_code, ")")
-        sys.exit()
-
-#Some other initialisation stuff
-listener = Listener()
-stream = Stream(auth, listener)
-
-try:
-    print('Streaming started!')
-    #Filter user
-    #stream.filter(track=['github'])
-    stream.filter(follow=[userID])  # user ID for random account
-
-except KeyboardInterrupt as e :
-    print("Stopped.")
-finally:
-    print('Done.')
-    stream.disconnect()
-    output.close()
-
-"""
