38cd8eafaa16d716f95248a3152a1f80cf16db00
on-patrol <74320696+on-patrol@users.noreply.github.com>
2020-11-27 14:14:25 +0000

    Update tweetPoster.py

--- tweetPoster.py
+++ tweetPoster.py
@@ -1,4 +1,4 @@
-iimport tweepy
+import tweepy
 import time
 from datetime import datetime
 
@@ -11,15 +11,18 @@ auth.set_access_token("1329343847902666752-FSvOVAVxwapGkgo0Y3vi3M6cgwBGKn",
 api = tweepy.API(auth, wait_on_rate_limit_notify=True)
 
 def tweetOnce(tweetText1):
-    print("Tweeted -", tweetText1,"-", (datetime.now()).strftime("%d/%m/%Y %H:%M:%S"))
+    #print("Tweeted -", tweetText1,"-", (datetime.now()).strftime("%d/%m/%Y %H:%M:%S"))
+    print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S")," : Time of Tweet")
     api.update_status((tweetText1))
     #print("Sleeping 6s")
     time.sleep(6)
 
 def tweetTwice(tweetText1, tweetText2):
     api.update_status((tweetText1))
-    print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S"), "Tweeted -", tweetText1,"-", )
+    #print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S"), "Tweeted -", tweetText1,"-", )
+    print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S")," : Time of Tweet")
     api.update_status((tweetText2))
-    print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S"), "Tweeted -", tweetText2,"-", )
+    print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S")," : Time of Tweet")
+    #print((datetime.now()).strftime("%d/%m/%Y %H:%M:%S"), "Tweeted -", tweetText2,"-", )
     #print("Sleeping 6s")
     time.sleep(6)
